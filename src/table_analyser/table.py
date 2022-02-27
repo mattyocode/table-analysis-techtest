5d71829e131cecdf5421dca5067288baef6973de
Matt <63904102+mattyocode@users.noreply.github.com>
2022-02-27 14:14:33 +0000

    feat: print table to console evenly spaced

--- src/table_analyser/table.py
+++ src/table_analyser/table.py
@@ -4,11 +4,16 @@ import operator
 
 class Table():
     def __init__(self, headers, rows):
+        self.cellwidth = 0
         self._headers = headers
+        for header in headers:
+            self.cellwidth = self.longest_cellwidth(header)
         self._rows = []
         if len(rows) > 0:
             for row in rows:
                 self._rows.append(row)
+                for cell in row:
+                    self.cellwidth = self.longest_cellwidth(cell)
 
     @property
     def headers(self):
@@ -17,3 +22,18 @@ class Table():
     @property
     def rows(self):
         return self._rows
+
+    def longest_cellwidth(self, cell):
+        return max(len(str(cell)), self.cellwidth)
+
+    def __str__(self):
+        if len(self._headers) == 0 and len(self._rows) == 0:
+            return "No table data."
+        formatted = []
+        if len(self._headers) > 0:
+            formatted_headers = "|".join([f"{header:{self.cellwidth}}" for header in self._headers])
+            formatted.append(formatted_headers)
+            formatted.append("-" * len(formatted_headers))
+        for row in self.rows:
+            formatted.append("|".join([f"{cell:{self.cellwidth}}" for cell in row]))
+        return "\n".join(formatted)
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,3 +1,4 @@
+from cgi import test
 import pytest
 
 from table_analyser.table import Table
@@ -29,10 +30,69 @@ def test_initalise_table_with_multi_column_multi_row_test_data():
     assert len(table.rows) == 2
 
 
-def test_initialise_empty_table_with_empty_arrays():
+def test_initialise_table_with_empty_input_arrays():
     test_headers = []
     test_row = []
     table = Table(test_headers, test_row)
     assert table.headers == test_headers
     assert table.rows == test_row
 
+
+def test_table_prints_to_console_evenly_spaced_single_column_long_header():
+    long_header = "Col 1"
+    test_header = [long_header]
+    test_row = [["a"]]
+    table = Table(test_header, test_row)
+    assert str(table) == "Col 1\n-----\na    "
+    for line in str(table).split("\n"):
+        assert len(line) == len(long_header)
+
+
+def test_table_prints_to_console_evenly_spaced_multi_column_long_header():
+    long_header = "Col Two"
+    test_headers = ["Col 1", long_header]
+    test_row = [["a", "bb"]]
+    table = Table(test_headers, test_row)
+    assert str(table) == "Col 1  |Col Two\n---------------\na      |bb     "
+    for line in str(table).split("\n"):
+        assert len(line) == len(long_header) * 2 + 1
+
+
+def test_table_prints_to_console_evenly_spaced_single_column_long_row():
+    long_row = "long row value"
+    test_header = ["Col 1"]
+    test_row = [[long_row]]
+    table = Table(test_header, test_row)
+    assert str(table) == "Col 1         \n--------------\nlong row value"
+    for line in str(table).split("\n"):
+        assert len(line) == len(long_row)
+
+
+def test_table_prints_to_console_evenly_spaced_multi_column_long_row():
+    long_row = "long row value"
+    test_headers = ["Col 1", "Col 2"]
+    test_row = [["a", long_row]]
+    table = Table(test_headers, test_row)
+    for line in str(table).split("\n"):
+        assert len(line) == len(long_row) * 2 + 1
+
+
+def test_table_prints_to_console_with_empty_input_arrays():
+    test_headers = []
+    test_row = []
+    table = Table(test_headers, test_row)
+    assert str(table) == "No table data."
+
+
+def test_table_prints_to_console_with_empty_headers_array():
+    test_headers = []
+    test_row = [["a", "b"]]
+    table = Table(test_headers, test_row)
+    assert str(table) == "a|b"
+
+
+def test_table_prints_to_console_with_empty_rows_array():
+    test_headers = ["Col 1", "Col 2"]
+    test_row = []
+    table = Table(test_headers, test_row)
+    assert str(table) == "Col 1|Col 2\n-----------"
