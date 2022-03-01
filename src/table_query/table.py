8459895d0fe4dab65685f22d8105b86e0ee8d308
Matt <63904102+mattyocode@users.noreply.github.com>
2022-03-01 19:56:06 +0000

    doc string amends

--- src/table_query/table.py
+++ src/table_query/table.py
@@ -17,6 +17,13 @@ class Table:
                 self._rows.append(row)
                 self.set_column_cell_widths(row)
 
+    def set_column_cell_widths(self, cells):
+        """Iterate through array and set cellwidths dictionary to 
+        max width of each column. Helper function for initialisation."""
+        for i, cell in enumerate(cells):
+            current_column_cellwidth = self.cellwidths.get(i, 0)
+            self.cellwidths[i] = max(current_column_cellwidth, len(str(cell)))
+
     @property
     def headers(self):
         """Return table headers."""
@@ -33,14 +40,19 @@ class Table:
         return self._excluded_from_print
 
     def exclude_cols_from_print(self, columns):
+        """Add list of column names to attributes.
+        Column nanme is stored as index so it can be
+        used while iterating over both headers and rows.
+        """
         for column_name in columns:
             column_index = self._headers.index(column_name)
             self._excluded_from_print.append(column_index)
         return self._excluded_from_print
 
     def __str__(self):
-        """It prints each column neatly to length of longest value in column, \
-            i.e. column widths can differ from each other."""
+        """It returns table data in grid format, with each 
+        column the width of its longest cell.
+        """
         if len(self._headers) == 0 and len(self._rows) == 0:
             return "No table data."
         formatted = []
@@ -66,11 +78,6 @@ class Table:
             )
         return "\n".join(formatted)
 
-    def set_column_cell_widths(self, cells):
-        """Iterate through array and set cellwidths dictionary to max width of each column."""
-        for i, cell in enumerate(cells):
-            current_column_cellwidth = self.cellwidths.get(i, 0)
-            self.cellwidths[i] = max(current_column_cellwidth, len(str(cell)))
 
     def sort_by(self, column_name):
         """Return new table sorted by given column of float or int."""
@@ -79,19 +86,6 @@ class Table:
             self.headers, sorted(self.rows, key=lambda x: float(x[column_index]))
         )
 
-    def string_column_to_datetime(self, column_name):
-        """Return table with column converted to datetime object in place."""
-        column_index = self._headers.index(column_name)
-        for row in self._rows:
-            row[column_index] = datetime.strptime(row[column_index], "%d %b %Y").date()
-        return self
-
-    def datetime_column_to_string(self, column_name):
-        """Return table with column converted string format DD/MM/YYYY."""
-        column_index = self._headers.index(column_name)
-        for row in self._rows:
-            row[column_index] = datetime.strftime(row[column_index], "%d/%m/%Y")
-        return self
 
     def get_column_total(self, column_name):
         """Return total value for numerical column."""
@@ -102,9 +96,9 @@ class Table:
         return total
 
     def get_rows_equal_to(self, column_name, value):
-        """Return new table object with rows matching value. \
-            Using list comprehension here to meet requirements \
-            and as data set is small but implementing binary \
+        """Return new table object with rows matching value.
+            Using list comprehension here to meet requirements
+            and as data set is small but implementing binary
             search would be more performant."""
         column_index = self._headers.index(column_name)
         return Table(
@@ -123,3 +117,18 @@ class Table:
                 if row[column_index] >= start_value and row[column_index] <= end_value
             ],
         )
+
+    def string_column_to_datetime(self, column_name):
+        """Return table with column converted to datetime object in place."""
+        column_index = self._headers.index(column_name)
+        for row in self._rows:
+            row[column_index] = datetime.strptime(row[column_index], "%d %b %Y").date()
+        return self
+
+
+    def datetime_column_to_string(self, column_name):
+        """Return table with column converted string format DD/MM/YYYY."""
+        column_index = self._headers.index(column_name)
+        for row in self._rows:
+            row[column_index] = datetime.strftime(row[column_index], "%d/%m/%Y")
+        return self
