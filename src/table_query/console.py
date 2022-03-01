35360decd2983519d718891fbfe93efc3a290509
Matt <63904102+mattyocode@users.noreply.github.com>
2022-03-01 11:12:27 +0000

    console calls csvloader with file path

--- src/table_query/console.py
+++ src/table_query/console.py
@@ -1,7 +1,18 @@
 import click
 
+from .csv_loader import CSVLoader
+from .table import Table
+from .query_helper import QueryHelper
+
+def file_to_table(file_path):
+	csv_data = CSVLoader(file_path)
+	table = Table(csv_data.headers, csv_data.rows)
+	return table
+
 
 @click.command()
-def main():
+@click.option("--file-path", "-f", type=click.Path(exists=True), default="fixtures/Python Developer Test Dataset.csv")
+def main(file_path):
     """Table Analysis Python Developer test."""
+    table = file_to_table(file_path)
     click.echo("Hello world!")
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,5 @@
+import os
+
 import click.testing
 
 import pytest
@@ -7,9 +9,51 @@ from table_query import console
 
 @pytest.fixture
 def runner():
+    """Fixture for invoking command-line interfaces."""
     return click.testing.CliRunner()
 
 
+@pytest.fixture
+def mock_table(mocker):
+    """Fixture for mocking Table."""
+    return mocker.patch("table_query.console.Table")
+
+
+@pytest.fixture
+def mock_csvloader(mocker):
+    """Fixture for mocking CSVLoader."""
+    return mocker.patch("table_query.console.CSVLoader")
+
+
+@pytest.fixture
+def mock_queryhelper(mocker):
+    """Fixture for mocking QueryHelper."""
+    return mocker.patch("table_query.console.QueryHelper")
+
+
+@pytest.fixture(scope="session")
+def test_csv_file():
+    """It returns an empty TextIO file with csv extension."""
+    with open("test.csv", "w") as f:
+        yield f
+    os.remove("test.csv")
+
+
+@pytest.mark.e2e
 def test_main_succeeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_calls_csvloader_with_specified_path(
+    runner,
+    mock_csvloader,
+    test_csv_file,
+    # mock_table,
+    # mock_queryhelper,
+) -> None:
+    """It passes file path to csv loader instance."""
+    result = runner.invoke(console.main, ["--file-path=test.csv"])
+    print(result.__dict__)
+    assert result.exit_code == 0
+    mock_csvloader.assert_called_with("test.csv")
