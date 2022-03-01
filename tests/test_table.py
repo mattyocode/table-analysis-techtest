3037bbf0bb7b0242264014657c51abbe900f67cf
Matt <63904102+mattyocode@users.noreply.github.com>
2022-03-01 17:38:00 +0000

    fix table sort by issue - convert string to float

--- tests/test_table.py
+++ tests/test_table.py
@@ -149,22 +149,13 @@ def test_table_returns_new_table_with_same_order_when_integers_already_sorted():
     assert sorted_table.rows == [[5, 4], [1, 6]]
 
 
-def test_table_returns_new_table_sorted_by_string_column_value():
-    """It returns new table object sorted by string (ascending) in given column."""
-    test_headers = ["Col 1", "Col 2"]
-    test_rows = [["x", "y"], ["a", "b"]]
-    table = Table(test_headers, test_rows)
-    sorted_table = table.sort_by("Col 1")
-    assert sorted_table.rows == [["a", "b"], ["x", "y"]]
-
-
-def test_table_returns_new_table_with_same_order_when_strings_already_sorted():
-    """It returns new table object unchanged if given column of strings is already sorted."""
+def test_table_raises_valueerror_if_sort_by_called_on_non_numerical_string():
+    """It raises ValueError if string cannot be converted to float."""
     test_headers = ["Col 1", "Col 2"]
     test_rows = [["a", "b"], ["x", "y"]]
     table = Table(test_headers, test_rows)
-    sorted_table = table.sort_by("Col 1")
-    assert sorted_table.rows == [["a", "b"], ["x", "y"]]
+    with pytest.raises(ValueError):
+        table.sort_by("Col 1")
 
 
 def test_convert_date_string_to_datetime_object():
